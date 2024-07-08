bf9b85d22797f0d4a2c709abd31ef67e65b243e1
bragadeesh <bragadeesh03294@gamil.com>
2024-07-08 12:24:26 +0530

    first commit

---
+++ blog/form.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterFrom(UserCreationForm):
+    email=forms.EmailField(widget = forms.EmailInput (attrs={"placeholder": "Enter email-address", "class": "form-control"}),max_length=100,label='Email',required=True)
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={"placeholder": "Enter password", "class": "form-control"}))
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput (attrs={"placeholder": "Confirm password", "class": "form-control"}))
+    class Meta:
+        model = CustomUser
+        fields = ['email','password1','password2']
+
+
+class LoginForm(forms.Form):
+    email=forms.EmailField(widget = forms.EmailInput(attrs={"placeholder": "Enter email-address", "class": "form-control"}))
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={"placeholder": "Enter password", "class": "form-control"}))
+
+class PostFrom(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['Title','Content']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['Content']
